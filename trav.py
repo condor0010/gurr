3c1e06af10619e912c43b089d2e2b85b9da28c95
condor0010 <liatmp3@gmail.com>
2023-07-10 23:18:20 -0400

    cleaned up

--- trav.py
+++ trav.py
@@ -1,8 +1,17 @@
 import argparse
 import angr
 
+# Create an argument parser
+parser = argparse.ArgumentParser(description='CFG traversal with entry and exit nodes')
+parser.add_argument('binary', type=str, help='Path to the binary file to analyze')
+parser.add_argument('--start', type=str, help='Start node address in hex')
+parser.add_argument('--end', type=str, help='End node address in hex')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
 # Specify the binary file to analyze
-binary = './a.out'
+binary = args.binary
 
 # Create an angr project
 proj = angr.Project(binary, auto_load_libs=False)
@@ -10,31 +19,46 @@ proj = angr.Project(binary, auto_load_libs=False)
 # Perform the CFG analysis
 cfg = proj.analyses.CFG()
 
-# Create an argument parser
-parser = argparse.ArgumentParser(description='CFG traversal with entry and exit nodes')
-parser.add_argument('entry_node', type=str, help='Entry node address in hex')
-parser.add_argument('exit_node', type=str, help='Exit node address in hex')
+# Check if start address is specified, otherwise use the entry point
+if args.start:
+    start_node = int(args.start, 16)
+else:
+    start_node = proj.entry
 
-# Parse the command-line arguments
-args = parser.parse_args()
+# Get the start node of the CFG
+start_node = cfg.get_any_node(start_node)
 
-# Convert hexadecimal input to integer
-entry_node = int(args.entry_node, 16)
-exit_node = int(args.exit_node, 16)
+# Check if end address is specified
+if args.end:
+    end_node = int(args.end, 16)
+    end_node = cfg.get_any_node(end_node)
+else:
+    end_node = None
 
-# Get the entry node of the CFG
-entry_node = cfg.get_any_node(entry_node)
+# ANSI color codes
+COLOR_ENTRY = '\033[92m'
+COLOR_EXIT = '\033[91m'
+COLOR_VISITED = '\033[94m'
+COLOR_RESET = '\033[0m'
 
-def traverse(node=entry_node, visited=set(), indent_level=0):
+def traverse(node=start_node, visited=set(), indent_level=0):
     if node in visited:
         return
 
     visited.add(node)
-    symbol = proj.loader.find_symbol(node.addr)
-    symbol_name = symbol.name if symbol else ""
-    print(" " * indent_level, hex(node.addr), symbol_name)
+    addr = hex(node.addr)
 
-    if node.addr == exit_node:
+    # Apply color based on node type
+    if node.addr == start_node.addr:
+        addr = COLOR_ENTRY + addr + COLOR_RESET
+    elif node == end_node:
+        addr = COLOR_EXIT + addr + COLOR_RESET
+    elif node in visited:
+        addr = COLOR_VISITED + addr + COLOR_RESET
+
+    print(" " * indent_level, addr)
+
+    if node == end_node:
         return
 
     if node.successors:
@@ -42,3 +66,5 @@ def traverse(node=entry_node, visited=set(), indent_level=0):
             traverse(suc, visited, indent_level + 1)
 
 traverse()
+
+
