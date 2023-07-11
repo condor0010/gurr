b4cb45b077a4eaaee313441a105b5f442441bdf5
condor0010 <liatmp3@gmail.com>
2023-07-10 22:54:02 -0400

    takes args, still want to format

--- trav.py
+++ trav.py
@@ -1,3 +1,4 @@
+import argparse
 import angr
 
 # Specify the binary file to analyze
@@ -9,8 +10,20 @@ proj = angr.Project(binary, auto_load_libs=False)
 # Perform the CFG analysis
 cfg = proj.analyses.CFG()
 
+# Create an argument parser
+parser = argparse.ArgumentParser(description='CFG traversal with entry and exit nodes')
+parser.add_argument('entry_node', type=str, help='Entry node address in hex')
+parser.add_argument('exit_node', type=str, help='Exit node address in hex')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Convert hexadecimal input to integer
+entry_node = int(args.entry_node, 16)
+exit_node = int(args.exit_node, 16)
+
 # Get the entry node of the CFG
-entry_node = cfg.get_any_node(proj.entry)
+entry_node = cfg.get_any_node(entry_node)
 
 def traverse(node=entry_node, visited=set(), indent_level=0):
     if node in visited:
@@ -21,9 +34,11 @@ def traverse(node=entry_node, visited=set(), indent_level=0):
     symbol_name = symbol.name if symbol else ""
     print(" " * indent_level, hex(node.addr), symbol_name)
 
+    if node.addr == exit_node:
+        return
+
     if node.successors:
         for suc in node.successors:
             traverse(suc, visited, indent_level + 1)
 
 traverse()
-
